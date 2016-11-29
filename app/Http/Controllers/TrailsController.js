0002135755891aa13421b840d989a233cb5820c0
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-29 16:19:03 -0500

    added delete method

--- app/Http/Controllers/TrailsController.js
+++ app/Http/Controllers/TrailsController.js
@@ -28,13 +28,31 @@ class TrailsController {
 			}
 	}
 
+	* delete(request, response){
+		let data = request.only('title', 'waypoints') // get new data
+		let trail_id = request.param("trail_id") // get id of current trail
+		let trail = yield Trail.findBy('id', trail_id) // get current trail
+
+		if (!trail){
+			response.status(404).json({error: "Trail not found"})
+		} else {
+			const old_waypoints = yield Waypoint.query().table('waypoints')
+				.where('trail_id', trail_id)
+
+			for (var i=0; i<old_waypoints.length; i++){
+				let deletedWaypoint = yield Waypoint.find(old_waypoints[i].id)
+				yield deletedWaypoint.delete();
+			}
+			yield trail.delete();
+		}
+
+	}
+
 	* update(request, response){
 		let data = request.only('title', 'waypoints') // get new data
 		let trail_id = request.param("trail_id") // get id of current trail
 		let trail = yield Trail.findBy('id', trail_id) // get current trail
 		let exists = yield Trail.findBy('title', data.title) // check if title already exists in database
-
-		console.log(trail.title, data.title, exists)
 
 		if (!trail){
 			response.status(404).json({error: "Trail not found"})
